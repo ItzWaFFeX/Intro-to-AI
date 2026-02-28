227abb9a8def172783b33eb0fce499dab56522c4
ItzWaFFeX <164030036+ItzWaFFeX@users.noreply.github.com>
2026-02-28 07:55:31 -0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,41 @@
+#data types
+
+x = 67 #int
+y = 6.7 #float
+is_active = True #boolean
+name = "Ethan" #string
+
+#operators
+
+a = 20+20 #add
+b = 20-20 #sub
+c = 20*20 #mult
+d = 20/20 #div
+e = 10*10 #power
+
+#conditional statements
+
+x = 10
+
+if x > 0:
+    print("yes")
+elif x < 0:
+    print("No")
+else:
+    ("none")
+
+#loop
+
+for i in range (1,6):
+    print(i)
+
+#function
+
+def greet (name) :
+    print(f"Hello bro{name}")
+greet("Ethan")
+
+#
+
+my_list = [1,2,3,4,5,6,7,8,9]
+my_dictionary = {"name" : "Ethan" , "age" : 14}
